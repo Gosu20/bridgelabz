c3b869821741f43561574650542387057ac6bef4
Gosu20 <workwithananya20@gmail.com>
2025-08-25 11:42:49 +0530

    Add files via upload

---
+++ vowels.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class vowels {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your string: ");
+        String s=sc.nextLine().toLowerCase();
+        int count=0;
+        int len=s.length();
+        for(int i=0;i<len;i++){
+            if(s.charAt(i)=='a'|| s.charAt(i)=='e'|| s.charAt(i)=='i'|| s.charAt(i)=='o'|| s.charAt(i)=='u' ){
+                count++;
+            }
+        }
+        System.out.println("the total number of vowels in the given string are: " + count);
+
+    }
+    
+}
